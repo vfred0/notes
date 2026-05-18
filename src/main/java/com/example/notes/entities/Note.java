6571720de9ba35a30ccd214013c0f9a85fad681f
Victor Arreaga <varreagae@gnoboa.com>
2026-05-18 10:49:50 -0500

    feat(notes): added persistence

---
+++ src/main/java/com/example/notes/entities/Note.java
@@ -0,0 +1,23 @@
+package com.example.notes.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "notes")
+public class Note {
+
+    @Id
+    private String id;
+    private String title;
+    private String description;
+}
